1fa02e61b053d9d0e9e05f37bd142a242c1e6db0
Ousman Habib <uthmmanhabib@gmail.com>
2025-10-24 06:08:46 +0000

    In the order page new customer asks only name and email

--- src/hooks/use-customers.tsx
+++ src/hooks/use-customers.tsx
@@ -12,7 +12,7 @@ interface CustomerContextType {
   customers: Customer[];
   loading: boolean;
   getCustomerById: (id: string) => Customer | undefined;
-  addCustomer: (customer: Omit<Customer, 'id'| 'ownerId' | 'orderIds' | 'reviews'>) => Promise<string>;
+  addCustomer: (customer: Partial<Omit<Customer, 'id'| 'ownerId' | 'orderIds' | 'reviews'>> & { name: string, email: string }) => Promise<string>;
 }
 
 const CustomerContext = createContext<CustomerContextType | undefined>(undefined);
@@ -31,17 +31,23 @@ export function CustomerProvider({ children }: { children: ReactNode }) {
     return customers.find(customer => customer.id === id);
   };
   
-  const addCustomer = async (customerData: Omit<Customer, 'id'| 'ownerId' | 'orderIds' | 'reviews'>): Promise<string> => {
+  const addCustomer = async (customerData: Partial<Omit<Customer, 'id'| 'ownerId' | 'orderIds' | 'reviews'>> & { name: string, email: string }): Promise<string> => {
     if (!user) {
       throw new Error("User not available");
     };
     
     const newCustomer: Customer = {
-      ...customerData,
+      name: customerData.name,
+      email: customerData.email,
+      phoneNumbers: customerData.phoneNumbers || [{type: "Mobile", number: "Not provided"}],
+      gender: customerData.gender || 'Other',
+      location: customerData.location || { town: "Not specified" },
+      avatarUrl: customerData.avatarUrl || `https://i.pravatar.cc/150?u=${customerData.email || customerData.name}`,
       id: uuidv4(),
       ownerId: user.id,
       orderIds: [],
       reviews: [],
+      ...customerData,
     };
 
     setCustomers(prev => [newCustomer, ...prev]);
