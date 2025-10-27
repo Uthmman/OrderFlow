7d565a998ab037ded6a58d24e6131a19a79aa1ce
Ousman Habib <uthmmanhabib@gmail.com>
2025-10-27 09:14:46 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
+
 export const firebaseConfig = {
   "projectId": "course-registration-cce07",
   "appId": "1:117088413094:web:3aa53a2afe004846f65b67",
   "apiKey": "AIzaSyCOTBiGBKbvXQYK1YE4tBVupjiPACDpN0Y",
   "authDomain": "course-registration-cce07.firebaseapp.com",
+  "storageBucket": "course-registration-cce07.appspot.com",
   "measurementId": "",
   "messagingSenderId": "117088413094"
 };
